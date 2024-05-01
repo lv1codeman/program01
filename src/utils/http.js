3ad4c05c5cefdc7ce5b72ec359e5fa0f04e29483
lv1codeman <lv1codeman@gmail.com>
2024-05-01 11:35:18 +0800

    axios基礎封裝 apis/testAPI.js會導入utils/http.js
    在main.js呼叫apis/testAPI.js

--- src/utils/http.js
+++ src/utils/http.js
@@ -7,6 +7,21 @@ const httpInstance = axios.create({
   timeout: 5000
 })
 
-//攔截器
+// 攔截器
+
+// axios請求攔截器
+httpInstance.interceptors.request.use(
+  (config) => {
+    return config
+  },
+  (e) => Promise.reject(e)
+)
+// axios響應式攔截器
+httpInstance.interceptors.response.use(
+  (res) => res.data,
+  (e) => {
+    return Promise.reject(e)
+  }
+)
 
 export default httpInstance
