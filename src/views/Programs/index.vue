6585f482665e0c8fb0aae452ebe80b9570009063
lv1codeman <lv1codeman@gmail.com>
2024-05-07 01:34:22 +0800

    00

--- src/views/Programs/index.vue
+++ src/views/Programs/index.vue
@@ -202,11 +202,13 @@ const setStatus = (item) => {
 }
 @media screen and (max-width: 1200px) {
   .outouter-box {
+    // height: 900px;
     height: calc($baseNum * 9);
   }
 }
 @media screen and (max-width: 818px) {
   .outouter-box {
+    // height: 1700px;
     height: calc($baseNum * 17);
     justify-content: center;
   }
