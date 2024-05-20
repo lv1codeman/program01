78d59865c8f199cf1157eef2934fc67f41464014
lv1codeman <lv1codeman@gmail.com>
2024-05-21 00:16:35 +0800

    Updates

--- assets/404-B4ffSXkR.js
+++
@@ -1,1 +0,0 @@
-import{c as e,a as t,w as a,p as s,o as c,b as o}from"./index-B4aCSuQ1.js";const r={class:"page-container"},i={__name:"404",setup(n){return(_,l)=>(c(),e("div",r,[t(s,null,{default:a(()=>[o("404 頁面不存在")]),_:1})]))}};export{i as default};
