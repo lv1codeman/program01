9796c0a840a6270e2caced16a976c95d43e30f8e
lv1codeman <lv1codeman@gmail.com>
2024-05-21 12:12:52 +0800

    Updates

--- assets/404-_5HC6N1V.js
+++
@@ -1,1 +0,0 @@
-import{c as e,a as t,w as a,p as s,o as c,b as o}from"./index-CYtaR0dJ.js";const r={class:"page-container"},i={__name:"404",setup(n){return(_,l)=>(c(),e("div",r,[t(s,null,{default:a(()=>[o("404 頁面不存在")]),_:1})]))}};export{i as default};
