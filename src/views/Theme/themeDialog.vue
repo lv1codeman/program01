2083dfed523aef4916baa9a9486c2cc14a28f2ae
lv1codeman <lv1codeman@gmail.com>
2024-05-24 15:06:56 +0800

    css if else

--- src/views/Theme/themeDialog.vue
+++ src/views/Theme/themeDialog.vue
@@ -5,10 +5,12 @@ import generateShades from '@/utils/color/genColorGrade.js'
 const setColor = (target, colorCode) => {
   console.log('change primary color')
   const el = document.documentElement
-  console.log(getComputedStyle(el).getPropertyValue('--el-color-primary'))
+  // console.log(getComputedStyle(el).getPropertyValue('--el-color-primary'))
 
-  const shadesAndTints = generateShades(colorCode)
-  console.log(shadesAndTints)
+  const colorTable = generateShades(colorCode)
+  console.log(colorTable)
+
+  console.log('target=', target)
 
   //#region 建立色階表
   const postfix = []
@@ -20,11 +22,11 @@ const setColor = (target, colorCode) => {
     postfix[i] = target + '-dark-' + (i - 9)
   }
   // 第一個顏色放原本的色碼
-  shadesAndTints.unshift(colorCode)
+  colorTable.unshift(colorCode)
   //#endregion
 
   const target_list = []
-  for (let i = 0; i < postfix.length; i++) target_list.push({ key: postfix[i], value: shadesAndTints[i] })
+  for (let i = 0; i < postfix.length; i++) target_list.push({ key: postfix[i], value: colorTable[i] })
 
   console.log(target_list)
 
@@ -112,23 +114,33 @@ const predefineColors = ref(['#6bb1cc', '#1e90ff', 'rgba(255, 69, 0, 0.68)'])
             color-format="hex"
           />
         </div>
-        <div class="light9">light9</div>
-        <div class="light8">light8</div>
-        <div class="light7">light7</div>
-        <div class="light5">light5</div>
-        <div class="light3">light3</div>
-        <div class="origin">origin</div>
-        <div class="dark">dark</div>
+        <div class="primary-light9">light9</div>
+        <div class="primary-light8">light8</div>
+        <div class="primary-light7">light7</div>
+        <div class="primary-light5">light5</div>
+        <div class="primary-light3">light3</div>
+        <div class="primary-origin">origin</div>
+        <div class="primary-dark">dark</div>
       </div>
+
       <div class="title">成功色</div>
       <div class="description">--el-color-success</div>
-      <div class="palette">
-        <el-color-picker
-          v-model="successcolor"
-          :predefine="predefineColors"
-          @change="setColor('--el-color-success', primarycolor)"
-          color-format="hex"
-        />
+      <div class="showPalette">
+        <div class="palette">
+          <el-color-picker
+            v-model="successcolor"
+            :predefine="predefineColors"
+            @change="setColor('--el-color-success', successcolor)"
+            color-format="hex"
+          />
+        </div>
+        <div class="success-light9">light9</div>
+        <div class="success-light8">light8</div>
+        <div class="success-light7">light7</div>
+        <div class="success-light5">light5</div>
+        <div class="success-light3">light3</div>
+        <div class="success-origin">origin</div>
+        <div class="success-dark">dark</div>
       </div>
     </div>
 
@@ -165,6 +177,45 @@ const predefineColors = ref(['#6bb1cc', '#1e90ff', 'rgba(255, 69, 0, 0.68)'])
   </div>
 </template>
 <style lang="scss" scoped>
+// .showPalette {
+//   display: flex;
+//   margin: 5px 0;
+//   gap: 5px;
+//   justify-content: center;
+//   align-items: center;
+//   text-align: center;
+//   .palette {
+//     width: 50px;
+//   }
+//   .origin {
+//     background-color: var(--el-color-primary);
+//     width: 50px;
+//   }
+//   .light3 {
+//     background-color: var(--el-color-primary-light-3);
+//     width: 50px;
+//   }
+//   .light5 {
+//     background-color: var(--el-color-primary-light-5);
+//     width: 50px;
+//   }
+//   .light7 {
+//     background-color: var(--el-color-primary-light-7);
+//     width: 50px;
+//   }
+//   .light8 {
+//     background-color: var(--el-color-primary-light-8);
+//     width: 50px;
+//   }
+//   .light9 {
+//     background-color: var(--el-color-primary-light-9);
+//     width: 50px;
+//   }
+//   .dark {
+//     background-color: var(--el-color-primary-dark-2);
+//     width: 50px;
+//   }
+// }
 .showPalette {
   display: flex;
   margin: 5px 0;
@@ -172,36 +223,42 @@ const predefineColors = ref(['#6bb1cc', '#1e90ff', 'rgba(255, 69, 0, 0.68)'])
   justify-content: center;
   align-items: center;
   text-align: center;
-  .palette {
-    width: 50px;
+
+  $colors: (
+    primary: var(--el-color-primary),
+    success: var(--el-color-success)
+  );
+
+  @each $key, $value in $colors {
+    .#{$key}-origin {
+      background-color: if($key == 'primary', var(--el-color-primary), var(--el-color-success));
+      width: 50px;
+    }
   }
-  .origin {
-    background-color: var(--el-color-primary);
-    width: 50px;
+  @each $key, $value in $colors {
+    .#{$key}-dark {
+      background-color: if($key == 'primary', var(--el-color-primary-dark-2), var(--el-color-success-dark-2));
+      width: 50px;
+    }
   }
-  .light3 {
-    background-color: var(--el-color-primary-light-3);
-    width: 50px;
+
+  @for $i from 3 through 9 {
+    @each $key, $value in $colors {
+      .#{$key}-light#{$i} {
+        background-color: if(
+          $key == 'primary',
+          var(--el-color-primary-light-#{$i}),
+          var(--el-color-success-light-#{$i})
+        );
+        width: 50px;
+      }
+    }
   }
-  .light5 {
-    background-color: var(--el-color-primary-light-5);
-    width: 50px;
-  }
-  .light7 {
-    background-color: var(--el-color-primary-light-7);
-    width: 50px;
-  }
-  .light8 {
-    background-color: var(--el-color-primary-light-8);
-    width: 50px;
-  }
-  .light9 {
-    background-color: var(--el-color-primary-light-9);
-    width: 50px;
-  }
-  .dark {
-    background-color: var(--el-color-primary-dark-2);
-    width: 50px;
-  }
+  // @each $key, $value in $colors {
+  //   .#{$key}-dark#{$i} {
+  //     background-color: var(--el-color-primary-dark-2);
+  //     width: 50px;
+  //   }
+  // }
 }
 </style>
