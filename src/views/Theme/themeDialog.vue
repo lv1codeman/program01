05578030168e3b3100a74c43d60025c050ee2abe
lv1codeman <lv1codeman@gmail.com>
2024-05-24 15:50:15 +0800

    [workable]未設定樣板

--- src/views/Theme/themeDialog.vue
+++ src/views/Theme/themeDialog.vue
@@ -34,52 +34,14 @@ const setColor = (target, colorCode) => {
     getComputedStyle(el).getPropertyValue(item.key)
     el.style.setProperty(item.key, item.value)
   })
-
-  //#region
-  // const target_list = []
-  // target_list.push({ key: target, value: colorCode })
-  // target_list.push({
-  //   key: target + '-light-3',
-  //   value: rgbToHex_obj(genPrimaryLevel(hexToRgb(colorCode).r, hexToRgb(colorCode).g, hexToRgb(colorCode).b, 3))
-  // })
-  // target_list.push({
-  //   key: target + '-light-5',
-  //   value: rgbToHex_obj(genPrimaryLevel(hexToRgb(colorCode).r, hexToRgb(colorCode).g, hexToRgb(colorCode).b, 5))
-  // })
-  // target_list.push({
-  //   key: target + '-light-7',
-  //   value: rgbToHex_obj(genPrimaryLevel(hexToRgb(colorCode).r, hexToRgb(colorCode).g, hexToRgb(colorCode).b, 7))
-  // })
-  // target_list.push({
-  //   key: target + '-light-8',
-  //   value: rgbToHex_obj(genPrimaryLevel(hexToRgb(colorCode).r, hexToRgb(colorCode).g, hexToRgb(colorCode).b, 8))
-  // })
-  // target_list.push({
-  //   key: target + '-light-9',
-  //   value: rgbToHex_obj(genPrimaryLevel(hexToRgb(colorCode).r, hexToRgb(colorCode).g, hexToRgb(colorCode).b, 9))
-  // })
-  // target_list.push({
-  //   key: target + '-dark-2',
-  //   value: rgbToHex_obj(genPrimaryLevel(hexToRgb(colorCode).r, hexToRgb(colorCode).g, hexToRgb(colorCode).b, -2))
-  // })
-  //#endregion
-  //#region
-
-  // target_list.forEach((item) => {
-  //   getComputedStyle(el).getPropertyValue(item.key)
-  //   el.style.setProperty(item.key, item.value)
-  // })
-
-  //#endregion
-  // console.log(getComputedStyle(el).getPropertyValue('--el-color-error'))
-  // console.log(colorCode)
-
-  // 使用範例
-  // const hexColor = "#3498db";
-  // const shadesAndTints = generateShades(hexColor);
-  // console.log(shadesAndTints);
 }
 
+const setTextColor = (target, colorCode) => {
+  console.log('change primary color')
+  const el = document.documentElement
+  getComputedStyle(el).getPropertyValue(target)
+  el.style.setProperty(target, colorCode)
+}
 // $primaryColor: #6bb1cc;
 // $successColor: #ffbd41;
 // $warningColor: #ffeccc;
@@ -88,12 +50,14 @@ const setColor = (target, colorCode) => {
 // $infoColor: #8b8b8b;
 
 //#region 顏色變數宣告
-const primarycolor = ref('#6bb1cc')
-const successcolor = ref('#ffbd41')
-const warningcolor = ref('#ffeccc')
-const dangercolor = ref('#a33131')
-const infocolor = ref('#8b8b8b')
-const textprimarycolor = ref('#074b5f')
+const primaryColor = ref('#6bb1cc')
+const successColor = ref('#ffbd41')
+const warningColor = ref('#ffeccc')
+const dangerColor = ref('#a33131')
+const errorColor = ref('#d41414')
+
+const infoColor = ref('#8b8b8b')
+const textColor = ref('#074b5f')
 //#endregion
 
 const predefineColors = ref(['#6bb1cc', '#1e90ff', 'rgba(255, 69, 0, 0.68)'])
@@ -101,164 +65,201 @@ const predefineColors = ref(['#6bb1cc', '#1e90ff', 'rgba(255, 69, 0, 0.68)'])
 </script>
 <template>
   <div class="page-container">
-    <div class="themebox">
-      <div class="title">主色</div>
-      <div class="description">--el-color-primary</div>
-
-      <div class="showPalette">
-        <div class="palette">
-          <el-color-picker
-            v-model="primarycolor"
-            :predefine="predefineColors"
-            @change="setColor('--el-color-primary', primarycolor)"
-            color-format="hex"
-          />
+    <div class="themeBox">
+      <div class="colorBox">
+        <div class="title">主色 --el-color-primary</div>
+        <div class="showPalette-primary">
+          <div class="palette">
+            <el-color-picker
+              v-model="primaryColor"
+              :predefine="predefineColors"
+              @change="setColor('--el-color-primary', primaryColor)"
+              color-format="hex"
+            />
+          </div>
+          <div class="primary-light9">light9</div>
+          <div class="primary-light8">light8</div>
+          <div class="primary-light7">light7</div>
+          <div class="primary-light5">light5</div>
+          <div class="primary-light3">light3</div>
+          <div class="primary-origin">origin</div>
+          <div class="primary-dark">dark</div>
         </div>
-        <div class="primary-light9">light9</div>
-        <div class="primary-light8">light8</div>
-        <div class="primary-light7">light7</div>
-        <div class="primary-light5">light5</div>
-        <div class="primary-light3">light3</div>
-        <div class="primary-origin">origin</div>
-        <div class="primary-dark">dark</div>
       </div>
-
-      <div class="title">成功色</div>
-      <div class="description">--el-color-success</div>
-      <div class="showPalette">
-        <div class="palette">
-          <el-color-picker
-            v-model="successcolor"
-            :predefine="predefineColors"
-            @change="setColor('--el-color-success', successcolor)"
-            color-format="hex"
-          />
+      <el-divider />
+      <div class="colorBox">
+        <div class="title">成功色 --el-color-success</div>
+        <div class="showPalette-success">
+          <div class="palette">
+            <el-color-picker
+              v-model="successColor"
+              :predefine="predefineColors"
+              @change="setColor('--el-color-success', successColor)"
+              color-format="hex"
+            />
+          </div>
+          <div class="success-light9">light9</div>
+          <div class="success-light8">light8</div>
+          <div class="success-light7">light7</div>
+          <div class="success-light5">light5</div>
+          <div class="success-light3">light3</div>
+          <div class="success-origin">origin</div>
+          <div class="success-dark">dark</div>
         </div>
-        <div class="success-light9">light9</div>
-        <div class="success-light8">light8</div>
-        <div class="success-light7">light7</div>
-        <div class="success-light5">light5</div>
-        <div class="success-light3">light3</div>
-        <div class="success-origin">origin</div>
-        <div class="success-dark">dark</div>
       </div>
+      <el-divider />
+      <div class="colorBox">
+        <div class="title">警告色 --el-color-warning</div>
+        <div class="showPalette-warning">
+          <div class="palette">
+            <el-color-picker
+              v-model="warningColor"
+              :predefine="predefineColors"
+              @change="setColor('--el-color-warning', warningColor)"
+              color-format="hex"
+            />
+          </div>
+          <div class="warning-light9">light9</div>
+          <div class="warning-light8">light8</div>
+          <div class="warning-light7">light7</div>
+          <div class="warning-light5">light5</div>
+          <div class="warning-light3">light3</div>
+          <div class="warning-origin">origin</div>
+          <div class="warning-dark">dark</div>
+        </div>
+      </div>
+      <el-divider />
+
+      <div class="colorBox">
+        <div class="title">危險色 --el-color-danger</div>
+        <div class="showPalette-danger">
+          <div class="palette">
+            <el-color-picker
+              v-model="dangerColor"
+              :predefine="predefineColors"
+              @change="setColor('--el-color-danger', dangerColor)"
+              color-format="hex"
+            />
+          </div>
+          <div class="danger-light9">light9</div>
+          <div class="danger-light8">light8</div>
+          <div class="danger-light7">light7</div>
+          <div class="danger-light5">light5</div>
+          <div class="danger-light3">light3</div>
+          <div class="danger-origin">origin</div>
+          <div class="danger-dark">dark</div>
+        </div>
+      </div>
+      <el-divider />
+      <div class="colorBox">
+        <div class="title">錯誤色 --el-color-error</div>
+        <div class="showPalette-error">
+          <div class="palette">
+            <el-color-picker
+              v-model="errorColor"
+              :predefine="predefineColors"
+              @change="setColor('--el-color-error', errorColor)"
+              color-format="hex"
+            />
+          </div>
+          <div class="error-light9">light9</div>
+          <div class="error-light8">light8</div>
+          <div class="error-light7">light7</div>
+          <div class="error-light5">light5</div>
+          <div class="error-light3">light3</div>
+          <div class="error-origin">origin</div>
+          <div class="error-dark">dark</div>
+        </div>
+      </div>
+      <el-divider />
+      <div class="colorBox">
+        <div class="title">資訊色 --el-color-info</div>
+        <div class="showPalette-info">
+          <div class="palette">
+            <el-color-picker
+              v-model="infoColor"
+              :predefine="predefineColors"
+              @change="setColor('--el-color-info', infoColor)"
+              color-format="hex"
+            />
+          </div>
+          <div class="info-light9">light9</div>
+          <div class="info-light8">light8</div>
+          <div class="info-light7">light7</div>
+          <div class="info-light5">light5</div>
+          <div class="info-light3">light3</div>
+          <div class="info-origin">origin</div>
+          <div class="info-dark">dark</div>
+        </div>
+      </div>
+      <el-divider />
+      <div class="colorBox">
+        <div class="title">文字色 --el-text-color-regular</div>
+        <div class="showPalette-text">
+          <div class="palette">
+            <el-color-picker
+              v-model="textColor"
+              :predefine="predefineColors"
+              @change="setTextColor('--el-text-color-regular', textColor)"
+              color-format="hex"
+            />
+          </div>
+        </div>
+      </div>
+      <el-divider />
     </div>
-
-    <!-- 成功色success:<el-color-picker
-      v-model="successcolor"
-      show-alpha
-      :predefine="predefineColors"
-      @change="setPrimary(color)"
-    /><br />
-    警告色success:<el-color-picker
-      v-model="warningcolor"
-      show-alpha
-      :predefine="predefineColors"
-      @change="setPrimary(color)"
-    /><br />
-    危險色danger:<el-color-picker
-      v-model="dangercolor"
-      show-alpha
-      :predefine="predefineColors"
-      @change="setPrimary(color)"
-    /><br />
-    資訊色info:<el-color-picker
-      v-model="infocolor"
-      show-alpha
-      :predefine="predefineColors"
-      @change="setPrimary(color)"
-    /><br />
-    文字主色text-color-primary:<el-color-picker
-      v-model="textprimarycolor"
-      show-alpha
-      :predefine="predefineColors"
-      @change="setPrimary(color)"
-    /><br /> -->
   </div>
 </template>
 <style lang="scss" scoped>
-// .showPalette {
-//   display: flex;
-//   margin: 5px 0;
-//   gap: 5px;
-//   justify-content: center;
-//   align-items: center;
-//   text-align: center;
-//   .palette {
-//     width: 50px;
-//   }
-//   .origin {
-//     background-color: var(--el-color-primary);
-//     width: 50px;
-//   }
-//   .light3 {
-//     background-color: var(--el-color-primary-light-3);
-//     width: 50px;
-//   }
-//   .light5 {
-//     background-color: var(--el-color-primary-light-5);
-//     width: 50px;
-//   }
-//   .light7 {
-//     background-color: var(--el-color-primary-light-7);
-//     width: 50px;
-//   }
-//   .light8 {
-//     background-color: var(--el-color-primary-light-8);
-//     width: 50px;
-//   }
-//   .light9 {
-//     background-color: var(--el-color-primary-light-9);
-//     width: 50px;
-//   }
-//   .dark {
-//     background-color: var(--el-color-primary-dark-2);
-//     width: 50px;
-//   }
-// }
-.showPalette {
-  display: flex;
+.showPalette-text {
+  padding-left: 8px;
+}
+:deep(.el-divider--horizontal) {
   margin: 5px 0;
-  gap: 5px;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+}
+// Define color maps
+$colors: (
+  primary: var(--el-color-primary),
+  success: var(--el-color-success),
+  warning: var(--el-color-warning),
+  danger: var(--el-color-danger),
+  error: var(--el-color-error),
+  info: var(--el-color-info)
+);
 
-  $colors: (
-    primary: var(--el-color-primary),
-    success: var(--el-color-success)
-  );
+$lightLevels: (3, 5, 7, 8, 9);
+$darkLevel: 2;
 
-  @each $key, $value in $colors {
-    .#{$key}-origin {
-      background-color: if($key == 'primary', var(--el-color-primary), var(--el-color-success));
+// Generate classes
+@each $name, $value in $colors {
+  .showPalette-#{$name} {
+    display: flex;
+    margin: 5px 0;
+    gap: 5px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    .palette {
       width: 50px;
     }
-  }
-  @each $key, $value in $colors {
-    .#{$key}-dark {
-      background-color: if($key == 'primary', var(--el-color-primary-dark-2), var(--el-color-success-dark-2));
+
+    .#{$name}-origin {
+      background-color: $value;
       width: 50px;
     }
-  }
 
-  @for $i from 3 through 9 {
-    @each $key, $value in $colors {
-      .#{$key}-light#{$i} {
-        background-color: if(
-          $key == 'primary',
-          var(--el-color-primary-light-#{$i}),
-          var(--el-color-success-light-#{$i})
-        );
+    @each $level in $lightLevels {
+      .#{$name}-light#{$level} {
+        background-color: var(--el-color-#{$name}-light-#{$level});
         width: 50px;
       }
     }
+
+    .#{$name}-dark {
+      background-color: var(--el-color-#{$name}-dark-#{$darkLevel});
+      width: 50px;
+    }
   }
-  // @each $key, $value in $colors {
-  //   .#{$key}-dark#{$i} {
-  //     background-color: var(--el-color-primary-dark-2);
-  //     width: 50px;
-  //   }
-  // }
 }
 </style>
