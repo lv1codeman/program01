3490d6bce9af6ae6381164caac6e84e10cc88bfd
lv1codeman <lv1codeman@gmail.com>
2024-05-27 11:28:52 +0800

    改用icondrop

---
+++ src/components/icons/IconDrop.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    id="eLSR12W9CDs1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 100 100"
+    shape-rendering="geometricPrecision"
+    text-rendering="geometricPrecision"
+  >
+    <ellipse rx="24.941683" ry="24.941683" transform="translate(43.181403 56.818587)" stroke-width="0" />
+    <ellipse rx="9.869011" ry="9.869011" transform="translate(71.89127 31.876904)" stroke-width="0" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconChild_end'
+}
+</script>
+
+<style scoped>
+.set-icon-size {
+  /* width: 50px;
+  height: 50px;
+  fill: currentColor; */
+  /* fill: var(--el-color-success-light-3); */
+}
+</style>
--- src/views/Layout/components/LayoutPageTitle.vue
+++ src/views/Layout/components/LayoutPageTitle.vue
@@ -1,7 +1,8 @@
+<script setup>
+import IconDrop from '@/components/icons/IconDrop.vue'
+</script>
 <template>
-  <div class="pageTitle">
-    <slot />
-  </div>
+  <div class="pageTitle"><IconDrop class="set-icon-size"></IconDrop><slot /></div>
 </template>
 
 <style scoped lang="scss">
@@ -15,17 +16,28 @@
   letter-spacing: 2px;
   color: var(--el-text-color-primary);
 }
-.pageTitle::before {
-  content: '';
-  width: 100%;
-  height: 100%;
+.set-icon-size {
+  width: 100px;
+  height: 100px;
   position: absolute;
-  top: -5px;
-  left: 0;
-  filter: invert(91%) sepia(14%) saturate(209%) hue-rotate(159deg) brightness(100%) contrast(94%);
-  background: url('@/assets/images/drop.png') no-repeat;
-  background-size: contain;
+  top: -28px;
+  left: -16px;
   z-index: -1;
-  // border: 2px solid blue;
+  fill: var(--el-color-primary-light-7);
 }
+// .pageTitle::before {
+//   content: '';
+//   width: 100px;
+//   height: 100px;
+//   position: absolute;
+//   top: -30px;
+//   left: -20px;
+//   // filter: invert(91%) sepia(14%) saturate(209%) hue-rotate(159deg) brightness(100%) contrast(94%);
+//   fill: rgb(255, 0, 0);
+//   // background: url('@/assets/images/drop.png') no-repeat;
+//   background: url('@/assets/icon/drop.svg') no-repeat;
+//   background-size: contain;
+//   z-index: -1;
+//   // border: 2px solid blue;
+// }
 </style>
