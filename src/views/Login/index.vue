7da3c35539bd2f6954661d4d762b072943b1edc7
lv1codeman <lv1codeman@gmail.com>
2024-06-04 17:21:45 +0800

    01

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { fetchToken } from '@/utils/fetchToken.js'
 
-await fetchToken()
+const login = async () => {
+  await fetchToken()
+}
 </script>
 
 <template>
   <div>Login page</div>
   <RouterLink to="/">學程平台首頁</RouterLink>
-  <el-button></el-button>
+  <el-button type="primary" @click="login">登入</el-button>
 </template>
